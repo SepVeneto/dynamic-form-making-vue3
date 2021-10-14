bbbc89d25ff125d5cc76dec640bb2fcd67d1f113
SepVeneto <sepveneto@qq.com>
2021-10-14 11:00:38 +0800

    引入组件库

--- designer/src/main.ts
+++ designer/src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import VueStore, { key } from './store'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/theme-chalk/index.css'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/theme-chalk/index.css'
 import compMask from './components/mask'
 import DynamicForm from 'dynamic-form'
+import Basic from 'basic-components'
 createApp(App)
-  .use(ElementPlus)
+  // .use(ElementPlus)
+  .use(Basic)
   .use(VueStore, key)
   .use(DynamicForm)
   .component(compMask.name, compMask)
